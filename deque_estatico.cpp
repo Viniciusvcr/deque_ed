a87328b5d6c7aef8d3e2ec0db24cf39397b7f0e1
Vinícius Regatieri <viniciusvcr@hotmail.com>
2018-05-01 02:21:19 -0300

    Exercícios Finalizados

--- deque_estatico.cpp
+++ deque_estatico.cpp
@@ -54,8 +54,8 @@ int insere_esq(deque* d, item x){
 
 int remove_dir(deque* d, item* retorno){
 	if(!vazio(d)){
-		d->direta = anterior(d->direta);
-		*retorno = d->dados[d->direta];
+		d->direita = anterior(d->direita);
+		*retorno = d->dados[d->direita];
 		return 1;
 	}
 	return 0;
@@ -71,12 +71,104 @@ int remove_esq(deque* d, item* retorno){
 }
 
 void escreve_deque(deque* d){
-	for(int i=d->esquerda; i!=d->direta; i=proximo(i)){
+	for(int i=d->esquerda; i!=d->direita; i=proximo(i)){
 		cout << d->dados[i].chave << " ";
 	}
 	cout << endl;
 }
 
-int main(){
+void escreve_dir(deque* d){ //exercício 1
+	for(int i=anterior(d->direita); i!=anterior(d->esquerda); i=anterior(i)){
+		cout << d->dados[i].chave << " ";
+	}
+	cout << endl << endl;
+}
 
+int extremidade_dir(deque* d){ //exercício 2a
+	return d->dados[anterior(d->direita)].chave;
+	//caso o deque esteja vazio, um lixo de memória será retornado
+}
+
+int extremidade_esq(deque* d){ //exercício 2b
+	return d->dados[d->esquerda].chave;
+	//caso o deque esteja vazio, um lixo de memória será retornado
+}
+
+void clear_screen(){
+	system("clear");
+}
+
+int main(){
+	deque A;
+	int opt;
+	item insere, retorno;
+
+	inicializa(&A);
+	do{
+		fflush(stdin);
+		cout << "[1] Status do deque" << endl;
+		cout << "[2] Inserir na direita" << endl;
+		cout << "[3] Inserir na esquerda" << endl;
+		cout << "[4] Remover na direita" << endl;
+		cout << "[5] Remover na esquerda" << endl;
+		cout << "[6] Escrever direita p/ esquerda" << endl;
+		cout << "[7] Extremidade direita" << endl;
+		cout << "[8] Extremidade esquerda" << endl;
+		cin >> opt;
+		switch(opt){
+			case 1:
+				if(vazio(&A))
+					cout << "Deque Vazio!" << endl;
+				else cout << "Deque nao vazio" << endl;
+				
+				if(cheio(&A))
+					cout << "Deque cheio" << endl;
+				else cout << "Deque nao cheio" << endl;
+				cout << endl;
+			break;
+
+			case 2:
+				clear_screen();
+				cout << "Digite o elemento a ser inserido: ";
+				cin >> insere.chave;
+				if(insere_dir(&A, insere))
+					cout << "ELEMENTO INSERIDO COM SUCESSO!\n" << endl;
+				else cout << "LISTA CHEIA!\n" << endl;
+			break;
+
+			case 3:
+				clear_screen();
+				cout << "Digite o elemento a ser inserido: ";
+				cin >> insere.chave;
+				if(insere_esq(&A, insere))
+					cout << "ELEMENTO INSERIDO COM SUCESSO!\n" << endl;
+				else cout << "LISTA CHEIA\n" << endl;
+			break;
+
+			case 4:
+				if(remove_dir(&A, &retorno))
+					cout << retorno.chave << " REMOVIDO COM SUCESSO\n" << endl;
+				else cout << "ERRO NA REMOCAO\n" << endl;
+			break;
+
+			case 5:
+				if(remove_esq(&A, &retorno))
+					cout << retorno.chave << " REMOVIDO COM SUCESSO\n" << endl;
+				else cout << "ERRO NA REMOCAO\n" << endl;
+			break;
+
+			case 6:
+				cout << "DEQUE COMPLETO (DIR->ESQ):" << endl;
+				escreve_dir(&A);
+			break;
+
+			case 7:
+				cout << "Extremidade direita: " << extremidade_dir(&A) << endl << endl;
+			break;
+
+			case 8:
+				cout << "Extremidade esquerda: " << extremidade_esq(&A) << endl << endl;
+			break;
+		}
+	}while(opt != 0);
 }
